f11faab9e76ced52a7bd7860310292abf31a89ae
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2021-02-10 21:23:43 +0530

    yo

--- authroutes/authenticate.js
+++ authroutes/authenticate.js
@@ -2,24 +2,29 @@ const passport = require('passport');
 var a="";
 const fileupload=require('express-fileupload');
 const fs=require('fs');
+var FileManager = require('file-storage');
 require('../models/file')
 require('../models/student')
 require('../models/answer')
+var object1={};
 const mongoose=require('mongoose');
 const { Binary } = require('mongodb');
 const File=mongoose.model('files');
-const cors = require('cors');
+
+const answer=mongoose.model('answers');
 const student=mongoose.model('student');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 var customId=require("custom-id");
-const AWS=require('aws-sdk');
-const multer=require("multer");
 var axios=require("axios");
+const AWS=require('aws-sdk');
+const crypto=require("crypto");
+const multer=require("multer");
+const { CostExplorer } = require('aws-sdk');
 const mongouri="mongodb+srv://rahul:rahul@cluster0.rpfjy.mongodb.net/<dbname>?retryWrites=true&w=majority";
 const conn =mongoose.createConnection(mongouri);
 var id="";
-var object1={};
 const s3=new AWS.S3({
     accessKeyId:process.env.AWS_ID,
     secretAccessKey:process.env.AWS_SECRET
@@ -33,6 +38,7 @@ const storage=multer.memoryStorage({
   const upload = multer({ storage });
 module.exports = (app) => {
    app.use(fileupload());
+   app.set("view engine","ejs");
    app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true }));
    app.use(cors());
@@ -40,78 +46,53 @@ app.use(bodyParser.urlencoded({ extended: true }));
     app.get('/auth/google', passport.authenticate('google', {
         scope: ['email', 'profile']
     }));
-    app.post("/api/state",(req,res)=>{
-        console.log("This",req.body.profile);
-        a=req.body.profile;
-    })
+   
     app.get('/auth/google/callback', passport.authenticate('google'),(req,res)=>{
+        app.post("/api/state",(req,res)=>{
+            console.log("This",req.body.profile);
+            a=req.body.profile;
+        })
        if (a=="teacher"){
-        app.get('/api/output', (req, res) => {
-            
-        
-            info=req.user;
-            
-            res.send(req.user);
-            
-    
-        });
+       
+       
         res.redirect("/login");
-       }else{
+    }
+       if (a=="student"){
         app.get('/api/output1', (req, res) => {
-           
-            app.get('/api/output', (req, res) => {
-    
-               
-                
-                res.send("");
-                
-        
-            });   
+            console.log("wolabbi")
+            
             info=req.user;
             
             res.send(req.user);
             
-    
-        });
-       res.redirect("/paper");
-       }
         
+        });  
+    res.redirect("/paper");
     }
-        
-    );
-    app.get('/api/output', (req, res) => {
-        console.log("wolabbi")
-        
-        info=req.user;
-        
-        res.send(req.user);
-        
+
+ });
+ app.get('/api/output', (req, res) => {
+    console.log("wolabbi")
     
-    });   
+    info=req.user;
+    
+    res.send(req.user);
+    
+
+});   
+ 
+ 
     app.get("/api/logout", (req, res) => {
         req.logout();
-        app.get('/api/output', (req, res) => {
-
-        
-                
-                
+        app.get("/api/output",(req,res)=>{
             res.send("");
-            
-    
-        });
-        app.get('/api/output1', (req, res) => {
-
-        
-                
-                
-            res.send("");
-            
-    
-        });
+        })
         res.redirect("/");
     });
     var info="";
     
+   
+       
     app.post('/api/stack',(req,res)=>{
         var answers=req.body;
         
@@ -140,13 +121,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
         questions);
         
     })
+    var teacher_answers=[];
+    var student_answers=[];
+    app.post("/api/submit5",(req,res)=>{
+        console.log(req.body)
+        teacher_answers=req.body;
+    });
+    app.post("/api/answers",(req,res)=>{
+        console.log(req.body)
+        student_answers=req.body
+    })  
+    
     app.post('/api/submit',(req,res)=>{
         
         
         console.log("wola",req.files.file);
+                  
         const file=req.files.file;
-        var id=customId(file);
-        console.log("file",file);
+        id=customId(file);
         upload.single(file);
         const params={
                 Bucket:"examanandvemuri1",
@@ -157,10 +149,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
         }
         s3.upload(params,(error,data)=>{
                 if(error){
-                    res.status(500).send(error);
+                    console.log(error)
                 }
                 res.status(200).send(data);
         })
+        console.log("file",file);
         File.findOne({_id:info.googleId}).then((existingUser)=>{
             if(existingUser){
                 console.log("in to delete");
@@ -171,37 +164,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
             }
             if(info.googleId){
                 console.log("wolab",{googleId:info.googleId,email:info.email[0].value,name:file.name,files:file});
-                new File({_id:info.googleId,email:info.email[0].value,name:file.name,files:file,questions:q.questions,pdf_id:id}).save();
+                new File({_id:info.googleId,email:info.email[0].value,name:file.name,files:file,questions:q.questions,pdf_id:id,answers:teacher_answers}).save();
             }
             
           
         })
-         
-          file.mv(`/client1/build/media/${file.name}`,err=>{
-            if(err){
-                console.log(err);
-                return res.status(500).send(err);
-            }
-        });
+       
+          
       
       
         
 
 })
 var googleId="";
-app.post("/api/submit4",(req,res)=>{
-    googleId=req.body.id;
-    console.log("This id",googleId);
-})
-app.post("/api/answers",(req,res)=>{
-    console.log(req.body);
-})  
+
+
+
+var teacher_answers1=[];
 app.post("/api/submit3",(req,res)=>{
     googleId=req.body.id;
     console.log("This id",googleId);
     console.log(googleId)
     var name1="";
-    
     if(info.googleId!=undefined){
     File.findOne({pdf_id:googleId},(err,user)=>{
         if(user!=null){
@@ -243,38 +227,43 @@ app.post("/api/submit3",(req,res)=>{
     })
 }
 })
+
+
+
 app.get("/api/submit3",(req,res)=>{
     res.send(object1);
 })
-var score=0;
-
- 
     
 
         
     
-app.get("/api/score",(req,res)=>{
-    var score=0;
-    console.log("teacher",teacher_answers1,student_answers);
-    for(var i=0;i<teacher_answers1.length;i++){
-        for(var j=0;j<student_answers.length;j++){
-            if(teacher_answers1[i].q_no==student_answers[j].q_no){
-                console.log(teacher_answers1[i].q_no,student_answers[i].q_no)
-                if(teacher_answers1[i].answer==student_answers[j].answer)
-                    score=score+1;
+    app.get("/api/score",(req,res)=>{
+        var score=0;
+        console.log("teacher",teacher_answers1,student_answers);
+        for(var i=0;i<teacher_answers1.length;i++){
+            for(var j=0;j<student_answers.length;j++){
+                if(teacher_answers1[i].q_no==student_answers[j].q_no){
+                    console.log(teacher_answers1[i].q_no,student_answers[i].q_no)
+                    if(teacher_answers1[i].answer==student_answers[j].answer)
+                        score=score+1;
+                }
             }
         }
-    }
-    answer.findOne({_id:info.googleId,pdf_id:googleId}).then((user)=>{
-        if(user){
-            res.send("Sorry you have already given the test");
-        }else{
-            new answer({_id:info.googleId,email:info.email[0].value,pdf_id:googleId,student_score:score,answers:student_answers}).save().then(()=>{
-                res.send("Thank you for giving the test");
-            });
-        }
-    })
-  
+        answer.findOne({_id:info.googleId,pdf_id:googleId}).then((user)=>{
+            if(user){
+                res.send("Sorry you have already given the test");
+            }else{
+                new answer({_id:info.googleId,email:info.email[0].value,pdf_id:googleId,student_score:score,answers:student_answers}).save().then(()=>{
+                    res.send("Thank you for giving the test");
+                });
+            }
+        })
+      
 })
 
+
 }
+    
+
+
+
