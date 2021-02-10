dc5fd54fa9edd85ee21c7dcd942e4f30e466d4d5
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2021-02-10 23:58:03 +0530

    yo

--- authroutes/authenticate.js
+++ authroutes/authenticate.js
@@ -10,7 +10,7 @@ const mongoose=require('mongoose');
 const { Binary } = require('mongodb');
 const File=mongoose.model('files');
 const cors = require('cors');
-
+const student=mongoose.model('student')
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 var customId=require("custom-id");
@@ -189,7 +189,7 @@ app.post("/api/submit3",(req,res)=>{
             console.log(existingUser);
            
                 console.log("in")
-                
+                new student({_id:info.googleId,email:info.email[0].value,pdf_id:googleId}).save();
                 const params={
                     Bucket:"examanandvemuri1",
                     Key:user.pdf_id
